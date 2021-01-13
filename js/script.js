a35a8369d15bc1110b6c7b2cf211e52591f0fae5
Barbara Pakula <barbara.pakula@hotmail.com>
2021-01-14 00:09:19 +0100

    js  remove class 'active' from all articles

--- js/script.js
+++ js/script.js
@@ -20,22 +20,29 @@ const titleClickHandler = function (event) {
     console.log(event);
 
     /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
 
     /* add class 'active' to the clicked link */
 
     /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts .active');
 
-    /* get 'href' attribute from the clicked link */
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
+        /* get 'href' attribute from the clicked link */
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+        /* find the correct article using the selector (value of 'href' attribute) */
 
-    /* add class 'active' to the correct article */
-}
+        /* add class 'active' to the correct article */
+    }
 
-const links = document.querySelectorAll('.titles a');
+    const links = document.querySelectorAll('.titles a');
 
-for (let link of links) {
-    link.addEventListener('click', titleClickHandler);
-}
-
-
+    for (let link of links) {
+        link.addEventListener('click', titleClickHandler);
+    }
